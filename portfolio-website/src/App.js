d599476d5bda014981c8cd095946b6b66771098c
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-10-06 00:27:32 +0530

    Header

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,16 +1,29 @@
 import './App.css';
 import profileImage from './assets/images/man.png'; 
+import logoImage from './assets/images/newlogo.png'; 
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <div className="logo-section">
+          <img src={logoImage} alt="Logo" className="logo-image" />
+          <div className="name-title">
+            <p className="name">Vishwa Wijesakare</p>
+            <p className="title">Undergraduate</p>
+          </div>
+        </div>
+        <div className="contact-info">
+          <p className="contact">Email: araliyawijesakare@gmail.com | Phone: +94 70 233 1284</p>
+        </div>
+      </header>
       <div className="first-section">
         <div>
           <h1 className="News">Hello, I'm Vishwa!</h1>
           <p className="under">- Tech enthusiast -</p>
           <button className="contact-btn">Contact Me</button>
         </div>
-        <img src={profileImage} alt="Profile" className="profile-image" /> {/* Profile image on the right */}
+        <img src={profileImage} alt="Profile" className="profile-image" />
       </div>
     </div>
   );
