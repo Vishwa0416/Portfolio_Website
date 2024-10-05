923d88cb58e257c7280707f66c2eb69610f53f86
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-10-05 22:02:12 +0530

    Images update

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import profileImage from './assets/images/man.png'; 
 
 function App() {
   return (
     <div className="App">
       <div className="first-section">
-        <p className="News">Hello, I'm Vishwa</p>
+        <div>
+          <h1 className="News">Hello, I'm Vishwa!</h1>
+          <p className="under">- Tech enthusiast -</p>
+          <button className="contact-btn">Contact Me</button>
+        </div>
+        <img src={profileImage} alt="Profile" className="profile-image" /> {/* Profile image on the right */}
       </div>
     </div>
   );
