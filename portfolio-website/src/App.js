955fb801a62b0158a9f12b920c67b1b2cd17cf04
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-10-05 19:39:50 +0530

    New portfolio

---
+++ portfolio-website/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  <title>Vishwa Wijesekare</title>
+  return (
+    <div className="App">
+       <p className = "News">Hello, I'm Vishwa</p>
+    </div>
+  );
+}
+
+export default App;
