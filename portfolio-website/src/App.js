08682e6cb6ca69b0ff82ed4a0ad8f907feae13e5
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-10-10 20:33:30 +0530

    Scroll Down

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -35,15 +35,21 @@ function App() {
     };
   }, []);
 
+  // Scroll to second section
   const scrollToNextSection = () => {
     const nextSection = document.querySelector('.text-and-image-container');
     nextSection.scrollIntoView({ behavior: 'smooth' });
   };
 
+  // Scroll to third section
+  const scrollToThirdSection = () => {
+    const thirdSection = document.querySelector('.third-section');
+    thirdSection.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div className="App">
       <header>
-      {/*First Section*/ }
         <div className="logo-section">
           <img src={logoImage} alt="Logo" className="logo-image" />
           <div className="name-title">
@@ -73,8 +79,7 @@ function App() {
         </div>
       </div>
 
-      {/*Second Section*/ }
-
+      {/* Second Section */}
       <div className="text-and-image-container fade-on-scroll">
         <img src={manImage} alt="Man" className="man2-image" />
         <div className="text-section">
@@ -96,41 +101,39 @@ function App() {
             <button className="btn-secondary">Download CV</button>
           </div>
         </div>
-      </div>
-
-      {/*Third Section*/ }
-
-      <div className='third-section'>
-      <div className="text-and-image-container fade-on-scroll">
-      <div className="text-section">
-          <h1 className="News2">My Projects</h1>
-          <p>I have developed sereval projects with different software tools. Here are some of them to you to check out.</p>
-          <ul className="skills-list">
-            <h2><li><FontAwesomeIcon icon={faCheck} className="black-tick" /> AgriConnect (Group)</li></h2>
-            <p>Designed and implemented a Crop Recommendation Tool using AI algorithms, providing personalized crop suggestions based on location, soil, and climate conditions. 
-              Developed a Marketplace Platform allowing farmers to list produce, negotiate prices, and connect directly with buyers, eliminating intermediaries and ensuring fair compensation. </p>
-            <h2><li><FontAwesomeIcon icon={faCheck} className="black-tick" /> MedAssist (Group)</li></h2>
-            <p>MedAssist is an AI-powered mobile app that simplifies medical report interpretation and offers personalized healthcare recommendations. 
-              There are some other features as well. Such as, doctor recommendation system, AI chatbot for personal assistance, user feedback portal.
-            </p>
-            <h2><li><FontAwesomeIcon icon={faCheck} className="black-tick" /> TravPlan (Individiual)</li></h2>
-            <p>TravPlan is a Flutter-based travel app that helps you explore nearby destinations, manage bookings and travel dates, and discover popular places, all in one user-friendly platform. 
-              The application is based on Flutter framework.
-            </p>
-            <h2><li><FontAwesomeIcon icon={faCheck} className="black-tick" /> Drinker (Individiual)</li></h2>
-            <p>Drinker is a web app that enhances bar experiences with tailored features for owners, bartenders, and patrons.The project is based on Laravel and MySQL. </p>
-            <h2><li><FontAwesomeIcon icon={faCheck} className="black-tick" /> Glova (Group)</li></h2>
-            <p>Glova is a revolutionary skincare application that redefines the beauty industry by integrating advanced facial recognition technology and artificial intelligence. 
-              Designed to provide personalized skincare solutions tailored to individual skin types and conditions, Glova offers an innovative approach to skincare.</p>
-          </ul>
+        <div className="scroll-down2" onClick={scrollToThirdSection}>
+          <FontAwesomeIcon icon={faChevronDown} className="scroll-icon2" />
         </div>
-        <img src={man3Image} alt="Coder Image" className="man3-image" />
-        
       </div>
+
+      {/* Third Section */}
+      <div className='third-section'>
+        <div className="text-and-image-container fade-on-scroll">
+          <div className="text-section">
+            <h1 className="News2">My Projects</h1>
+            <p>I have developed several projects with different software tools. Here are some of them to check out.</p>
+            <ul className="skills-list">
+              <h2><li><FontAwesomeIcon icon={faCheck} className="black-tick" /> AgriConnect (Group)</li></h2>
+              <p>Designed and implemented a Crop Recommendation Tool using AI algorithms, providing personalized crop suggestions based on location, soil, and climate conditions. 
+              Developed a Marketplace Platform allowing farmers to list produce, negotiate prices, and connect directly with buyers, eliminating intermediaries and ensuring fair compensation. </p>
+              <h2><li><FontAwesomeIcon icon={faCheck} className="black-tick" /> MedAssist (Group)</li></h2>
+              <p>MedAssist is an AI-powered mobile app that simplifies medical report interpretation and offers personalized healthcare recommendations. 
+              There are some other features as well. Such as, doctor recommendation system, AI chatbot for personal assistance, user feedback portal.
+              </p>
+              <h2><li><FontAwesomeIcon icon={faCheck} className="black-tick" /> TravPlan (Individual)</li></h2>
+              <p>TravPlan is a Flutter-based travel app that helps you explore nearby destinations, manage bookings and travel dates, and discover popular places, all in one user-friendly platform.</p>
+              <h2><li><FontAwesomeIcon icon={faCheck} className="black-tick" /> Drinker (Individual)</li></h2>
+              <p>Drinker is a web app that enhances bar experiences with tailored features for owners, bartenders, and patrons. The project is based on Laravel and MySQL.</p>
+              <h2><li><FontAwesomeIcon icon={faCheck} className="black-tick" /> Glova (Group)</li></h2>
+              <p>Glova is a revolutionary skincare application that integrates advanced facial recognition technology and artificial intelligence to provide personalized skincare solutions tailored to individual skin types.</p>
+            </ul>
+          </div>
+          <img src={man3Image} alt="Coder Image" className="man3-image" />
+        </div>
       </div>
 
       <footer className='footer-section'>
-      <div className="footer-content">
+        <div className="footer-content">
           <p>© 2024 Vishwa Wijesakare. All Rights Reserved.</p>
           <p>Developed by Vishwa Wijesakare</p>
           <div className="footer-socials">
@@ -147,3 +150,4 @@ function App() {
 }
 
 export default App;
+
